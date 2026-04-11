312ff1fbe8502bea5ec697968c5b4be3265e93c3
TanvirAnjum-299 <sinthiaibnat79@gmail.com>
2026-04-11 13:25:25 +0600

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,10 @@
+studentname="robert"#string
+age=15#integer
+is_Male=False#boolean
+weight=38.5#float
+print("Name:", studentname)
+print("Data Type of Name is:", type(studentname))
+print("After Type Casting...")
+age=str(age)
+print(age)
+print("Data Type of Age is:", type(age))
