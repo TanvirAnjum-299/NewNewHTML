04abb8e09f0269ad0150bf4910fd4c9d5f38b59a
TanvirAnjum-299 <sinthiaibnat79@gmail.com>
2026-04-18 13:21:16 +0600

    Add files via upload

---
+++ positivenegative.py
@@ -0,0 +1,7 @@
+number=int(input("Enter Number To Be Checked: "))
+if number>0:
+    print("This is a Positive Number")
+elif number<0:
+    print("This is a Negative Number")
+else:
+    print("This is 0")
