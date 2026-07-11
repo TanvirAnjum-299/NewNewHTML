2b48e1ee08cc21f38e6861e02fb20f8c67081a33
TanvirAnjum-299 <sinthiaibnat79@gmail.com>
2026-07-11 13:24:14 +0600

    Add files via upload

---
+++ reversedsrting.py
@@ -0,0 +1,6 @@
+inputstring=input("Please enter your own string:")
+reversedstring=('')
+for i in inputstring:
+    reversedstring=i+reversedstring
+print("\nThe original string=",inputstring)
+print(reversedstring)
