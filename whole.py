092d7fc4e2d2ee1bf1f725486a08a557d9ec4041
TanvirAnjum-299 <sinthiaibnat79@gmail.com>
2026-07-11 13:09:20 +0600

    Add files via upload

---
+++ whole.py
@@ -0,0 +1,7 @@
+#what is a whole number?
+#sum of whole number till 5
+wholeNumber=5
+sum=0
+for number in range(0,wholeNumber+1): #number=0,1,2,3,4,5
+    sum=sum+number
+print("The sum of whole number till 5 is",sum)
